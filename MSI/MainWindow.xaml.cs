69dbc2c5679e37e0e4d689851621b3b8dd589a93
Jakub Cieslik <jm.cieslik@gmail.com>
2015-04-23 21:03:35 +0200

    Nowy widok, opcje wyglądu.

--- MSI/MainWindow.xaml.cs
+++ MSI/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace MSI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
